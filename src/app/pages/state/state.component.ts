e05c10b367619d29a2ec11dda9ded657468235c4
paingthetko11 <paingthetko081@gmail.com>
2025-02-20 11:58:57 +0630

    t

--- src/app/pages/state/state.component.ts
+++ src/app/pages/state/state.component.ts
@@ -6,10 +6,12 @@ import { ButtonModule } from 'primeng/button';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-state',
-  imports: [TableModule, ButtonModule, IconFieldModule, InputIconModule,InputTextModule],
+  imports: [TableModule, ButtonModule, IconFieldModule, InputIconModule,InputTextModule,FormsModule,CommonModule],
   templateUrl: './state.component.html',
   styleUrl: './state.component.scss',
 })
