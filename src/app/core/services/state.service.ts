1eb12965dc56a8c3cb0847ca614940af93808bc5
paingthetko11 <paingthetko081@gmail.com>
2025-02-20 16:41:15 +0630

    crud

--- src/app/core/services/state.service.ts
+++ src/app/core/services/state.service.ts
@@ -3,15 +3,44 @@ import { Injectable } from '@angular/core';
 import { RootModel } from '../models/root.model';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
+import { StateModel } from '../models/state.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StateService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) {}
   get(): Observable<RootModel> {
     let url: string = `${environment.apiUrl}/api/State`;
     return this.http.get<RootModel>(url);
+  }
+
+  getbyID(id: number): Observable<RootModel> {
+    let url: string = `${environment.apiUrl}/api/State/${id}`;
+    return this.http.get<RootModel>(url);
+  }
+
+  create(model: StateModel): Observable<RootModel> {
+    let url: string = `${environment.apiUrl}/api/State`;
+    return this.http.post<RootModel>(url, JSON.stringify(model), {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  update(id: number, model: StateModel): Observable<RootModel> {
+    let url: string = `${environment.apiUrl}/api/State/${id}`;
+    return this.http.put<RootModel>(url, JSON.stringify(model), {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  delete(id: number): Observable<RootModel> {
+    let url: string = `${environment.apiUrl}/api/State/${id}`;
+    return this.http.delete<RootModel>(url);
   }
 }
--- src/app/pages/state/entry/entry.component.ts
+++ src/app/pages/state/entry/entry.component.ts
@@ -1,11 +1,75 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { StateComponent } from '../state.component';
+import { StateService } from '../../../core/services/state.service';
+import { StateModel } from '../../../core/models/state.model';
+import { ActivatedRoute } from '@angular/router';
+import { Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-entry',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    ButtonModule,
+  ],
   templateUrl: './entry.component.html',
-  styleUrl: './entry.component.scss'
+  styleUrl: './entry.component.scss',
 })
-export class EntryComponent {
+export class EntryComponent implements OnInit {
+  stateID: number = 0;
 
+  model!: StateModel;
+
+  constructor(
+    private stateservice: StateService,
+    private route: ActivatedRoute
+  ) {}
+
+  private formBuilder = inject(FormBuilder);
+
+  stateForm = this.formBuilder.group({
+    stateId: [0],
+    stateName: [''],
+    stateNameMm: [''],
+  });
+
+  ngOnInit(): void {
+    this.stateID = parseInt(this.route.snapshot.paramMap.get('id') ?? '');
+    console.log(this.stateID);
+
+    if (this.stateID > 0) {
+      this.stateservice.getbyID(this.stateID).subscribe((res) => {
+        this.model = res.data as StateModel;
+        console.log(this.model);
+
+        this.stateForm.controls.stateId.setValue(this.model.stateId);
+        this.stateForm.controls.stateId.disable();
+        this.stateForm.controls.stateName.setValue(this.model.stateName);
+        this.stateForm.controls.stateNameMm.setValue(this.model.stateNameMm);
+      });
+    }
+  }
+
+  submit(): void {
+    var model: StateModel = {
+      stateId: this.stateForm.controls.stateId.value ?? 0,
+      stateName: this.stateForm.controls.stateName.value ?? '',
+      stateNameMm: this.stateForm.controls.stateNameMm.value ?? '',
+    };
+    if (this.stateID > 0) {
+      this.stateservice.update(this.stateID,model).subscribe((res) => {
+        console.log(res);
+      });
+    } else {
+      this.stateservice.create(model).subscribe((res) => {
+        console.log(res);
+      });
+    }
+  }
 }
