287a01a9b91b702571295dd04019f4f9479f0f23
paingthetko11 <paingthetko081@gmail.com>
2025-02-25 12:14:05 +0630

    update code

--- src/app/pages/allowance/entry/entry.component.ts
+++ src/app/pages/allowance/entry/entry.component.ts
@@ -21,17 +21,17 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './entry.component.scss',
 })
 export class EntryComponent implements OnInit {
-  id: number = 0;
+  allowanceId: number = 0;
   model!: AllowaneModel;
+
   constructor(
-    private allowencesService: AllowanceService,
+    private allowancesService: AllowanceService,
     private route: ActivatedRoute,
     private datepipe: DatePipe
   ) {}
 
   private formBuilder = inject(FormBuilder);
-
-  allowenceForm = this.formBuilder.group({
+  allowanceForm = this.formBuilder.group({
     allowanceId: [0],
     companyId: [''],
     branchId: [0],
@@ -50,80 +50,94 @@ export class EntryComponent implements OnInit {
   });
 
   ngOnInit(): void {
-    this.id = parseInt(this.route.snapshot.paramMap.get('id') ?? '');
-    console.log(this.id);
-
-    if (this.id > 0) {
-      this.allowencesService.getbyID(this.id).subscribe((res) => {
+    this.allowanceId = parseInt(this.route.snapshot.paramMap.get('id') ?? '');
+    if (this.allowanceId > 0) {
+      this.allowancesService.getbyID(this.allowanceId).subscribe((res) => {
         this.model = res.data as AllowaneModel;
         console.log(this.model);
 
-        this.allowenceForm.controls.allowanceId.setValue(this.model.allowanceId);
-        this.allowenceForm.controls.allowanceId.disable();
-        this.allowenceForm.controls.companyId.setValue(this.model.companyId);
-        this.allowenceForm.controls.branchId.setValue(this.model.branchId);
-        this.allowenceForm.controls.deptId.setValue(this.model.deptId);
-        this.allowenceForm.controls.positionId.setValue(this.model.positionId);
-        this.allowenceForm.controls.allowanceName.setValue(
+        this.allowanceForm.controls.allowanceId.setValue(
+          this.model.allowanceId
+        );
+        this.allowanceForm.controls.allowanceId.disable();
+        this.allowanceForm.controls.companyId.setValue(this.model.companyId);
+        this.allowanceForm.controls.branchId.setValue(this.model.branchId);
+        this.allowanceForm.controls.companyId.setValue(this.model.companyId);
+        this.allowanceForm.controls.deptId.setValue(this.model.deptId);
+        this.allowanceForm.controls.positionId.setValue(this.model.positionId);
+        this.allowanceForm.controls.allowanceName.setValue(
           this.model.allowanceName
         );
-        this.allowenceForm.controls.description.setValue(this.model.description);
+        this.allowanceForm.controls.description.setValue(
+          this.model.description
+        );
+        this.allowanceForm.controls.status.setValue(this.model.status);
+        this.allowanceForm.controls.createdOn.setValue(
+          this.model.createdOn
+            ? this.datepipe.transform(this.model.createdOn, 'yyyy-MM-dd')
+            : null
+        );
 
-        this.allowenceForm.controls.status.setValue(this.model.status);
-        this.model.createdOn
-          ? this.datepipe.transform(this.model.createdOn, 'yyyy-MM-dd')
-          : null;
-        this.model.deletedBy
-          ? this.datepipe.transform(this.model.createdBy, 'yyyy-MM-dd')
-          : null;
-        this.allowenceForm.controls.updatedOn.setValue(this.model.updatedOn);
-        this.allowenceForm.controls.updatedBy.setValue(this.model.updatedBy);
-        this.allowenceForm.controls.deletedOn.setValue(this.model.deletedOn);
-        this.allowenceForm.controls.remark.setValue(this.model.remark);
+        this.allowanceForm.controls.createdBy.setValue(this.model.createdBy);
+        this.allowanceForm.controls.updatedOn.setValue(
+          this.model.updatedOn
+            ? this.datepipe.transform(this.model.updatedOn, 'yyyy-MM-dd')
+            : null
+        );
+
+        this.allowanceForm.controls.updatedBy.setValue(this.model.updatedBy);
+        this.allowanceForm.controls.deletedOn.setValue(
+          this.model.deletedOn
+            ? this.datepipe.transform(this.model.deletedOn, 'yyyy-MM-dd')
+            : null
+        );
+        this.allowanceForm.controls.deletedBy.setValue(this.model.deletedBy);
+        this.allowanceForm.controls.remark.setValue(this.model.remark);
       });
     }
-    
-    
   }
+
   submit(): void {
     let createdOn = this.datepipe.transform(
-      this.allowenceForm.controls.createdOn.value,
+      this.allowanceForm.controls.createdOn.value,
       'yyyy-MM-dd'
     );
 
     let updatedOn = this.datepipe.transform(
-      this.allowenceForm.controls.updatedOn.value,
+      this.allowanceForm.controls.updatedOn.value,
       'yyyy-MM-dd'
     );
 
     let deletedOn = this.datepipe.transform(
-      this.allowenceForm.controls.deletedOn.value,
+      this.allowanceForm.controls.deletedOn.value,
       'yyyy-MM-dd'
     );
-    var model : AllowaneModel = {
-      allowanceId: this.allowenceForm.controls.allowanceId.value?? 0,
-      companyId: this.allowenceForm.controls.companyId.value?? '',
-      branchId: this.allowenceForm.controls.branchId.value??0,
-      deptId: this.allowenceForm.controls.deptId.value??0,
-      positionId: this.allowenceForm.controls.positionId.value??0,
-      allowanceName: this.allowenceForm.controls.allowanceName.value?? '',
-      description: this.allowenceForm.controls.description.value?? '',
-      status: this.allowenceForm.controls.status.value?? true,
-      createdOn: createdOn?? '',
-      createdBy: this.allowenceForm.controls.createdBy.value?? '',
-      updatedOn: updatedOn?? '',
-      updatedBy: this.allowenceForm.controls.updatedBy.value?? '',
-      deletedOn: deletedOn?? '',
-      remark: this.allowenceForm.controls.remark.value?? '',
+    var model: AllowaneModel = {
+      allowanceId: this.allowanceForm.controls.allowanceId.value ?? 0,
+      companyId: this.allowanceForm.controls.companyId.value ?? '',
+      branchId: this.allowanceForm.controls.branchId.value ?? 0,
+      deptId: this.allowanceForm.controls.deptId.value ?? 0,
+      positionId: this.allowanceForm.controls.positionId.value ?? 0,
+      allowanceName: this.allowanceForm.controls.allowanceName.value ?? '',
+      description: this.allowanceForm.controls.description.value ?? '',
+      status: this.allowanceForm.controls.status.value ?? true,
+      createdOn: createdOn,
+      createdBy: this.allowanceForm.controls.createdBy.value ?? '',
+      updatedOn: updatedOn,
+      updatedBy: this.allowanceForm.controls.updatedOn.value ?? '',
+      deletedOn: deletedOn,
+      deletedBy: this.allowanceForm.controls.deletedOn.value ?? '',
+      remark: this.allowanceForm.controls.deletedBy.value ?? '',
     };
-    // if (this.id > 0) {
-    //   this.allowencesService.update(this.id,model).subscribe((res) => {
-    //     console.log(res);
-    //   });
-    // } else {
-    //   this.allowencesService.create(model).subscribe((res) => {
-    //     console.log(res);
-    //   });
-    // }
+
+    if (this.allowanceId > 0) {
+      this.allowancesService.update(this.allowanceId, model).subscribe((res) => {
+        console.log(res);
+      });
+    } else {
+      this.allowancesService.create(model).subscribe((res) => {
+        console.log(res);
+      });
+    }
   }
 }
