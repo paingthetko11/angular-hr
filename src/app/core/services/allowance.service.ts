93e78c366a4c829281377acb19decbba7aeb350b
paingthetko11 <paingthetko081@gmail.com>
2025-02-26 11:17:54 +0630

    update code

--- src/app/core/services/allowance.service.ts
+++ src/app/core/services/allowance.service.ts
@@ -18,6 +18,15 @@ export class AllowanceService {
     let url: string = `${environment.apiUrl}/api/Allowance/${id}`;
     return this.http.get<RootModel>(url);
   }
+  //  getByCB(companyId: string, branchId: number): Observable<RootModel> {
+  //   let url: string = `${GLBOAL.BASE_API_V1_URL}/master/allowance/by?`
+  //     + `companyId=${companyId}&branchId=${branchId}`;
+  //   return this.http.get<RootModel>(url);
+  // }
+  getByCB():Observable<RootModel>{
+    let url: string = `${environment.apiUrl}/api/Allowance/GetByCB`;
+    return this.http.get<RootModel>(url);
+  }
   create(model: AllowaneModel): Observable<RootModel> {
     let url: string = `${environment.apiUrl}/api/Allowance`;
     return this.http.post<RootModel>(url, JSON.stringify(model), {
