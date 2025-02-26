b6babd3e61610f9eb65249e3d841581ff5f51645
paingthetko11 <paingthetko081@gmail.com>
2025-02-26 15:42:11 +0630

    update code

--- src/app/core/services/allowance.service.ts
+++ src/app/core/services/allowance.service.ts
@@ -10,23 +10,28 @@ import { AllowaneModel } from '../models/allowane.model';
 })
 export class AllowanceService {
   constructor(private http: HttpClient) {}
+
   get(): Observable<RootModel> {
     let url: string = `${environment.apiUrl}/api/Allowance`;
     return this.http.get<RootModel>(url);
   }
+
   getbyID(id: number): Observable<RootModel> {
     let url: string = `${environment.apiUrl}/api/Allowance/${id}`;
     return this.http.get<RootModel>(url);
   }
+
   //  getByCB(companyId: string, branchId: number): Observable<RootModel> {
   //   let url: string = `${GLBOAL.BASE_API_V1_URL}/master/allowance/by?`
   //     + `companyId=${companyId}&branchId=${branchId}`;
   //   return this.http.get<RootModel>(url);
   // }
-  getByCB():Observable<RootModel>{
+
+  getByCB(): Observable<RootModel> {
     let url: string = `${environment.apiUrl}/api/Allowance/GetByCB`;
     return this.http.get<RootModel>(url);
   }
+
   create(model: AllowaneModel): Observable<RootModel> {
     let url: string = `${environment.apiUrl}/api/Allowance`;
     return this.http.post<RootModel>(url, JSON.stringify(model), {
@@ -35,15 +40,16 @@ export class AllowanceService {
       },
     });
   }
+
   update(id: number, model: AllowaneModel): Observable<RootModel> {
-    let url: string = `${environment.apiUrl}/api/Allowance
-/${id}`;
+    let url: string = `${environment.apiUrl}/api/Allowance/${id}`;
     return this.http.put<RootModel>(url, JSON.stringify(model), {
       headers: {
         'Content-Type': 'application/json',
       },
     });
   }
+  
   delete(id: number): Observable<RootModel> {
     let url: string = `${environment.apiUrl}/api/Allowance/${id}`;
     return this.http.delete<RootModel>(url);
