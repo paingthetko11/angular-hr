35956aae8f4c48733fc98b9dc974251b9cc55b8d
paingthetko11 <paingthetko081@gmail.com>
2025-02-27 16:35:19 +0630

    u.c

--- src/app/core/models/company.model.ts
+++ src/app/core/models/company.model.ts
@@ -1,27 +1,29 @@
-export interface Company {
-  companyId: string;
-  companyName: string;
-  joinDate: string;
-  licenseNo: string;
-  contactPerson: string;
-  primaryPhone: string;
-  otherPhone: string;
-  email: string;
-  houseNo: string;
-  streetId: number;
-  streetName: null;
-  townshipId: number;
-  stateId: number;
-  photo: null;
-  status: boolean;
-  createdOn: string;
-  createdBy: string;
-  updatedOn: null | string;
-  updatedBy: null | string;
-  deletedOn: null;
-  deletedBy: null;
-  remark: null;
-  hrBranches: any[];
-  hrLeaveGroups: any[];
-  hrLoanTypes: any[];
+export interface CompanyModel {
+    companyId: string;
+    companyName: string;
+    joinDate: string;
+    licenseNo: string;
+    contactPerson: string;
+    primaryPhone: string;
+    otherPhone: string;
+    email: string;
+    houseNo: string;
+    streetId: number;
+    streetName: string;
+    townshipId: number;
+    townshipName: string;
+    stateId: number;
+    stateName: string;
+    photo: null;
+    status: boolean;
+    createdOn: string;
+    createdBy: string;
+    updatedOn: null | string;
+    updatedBy: null | string;
+    deletedOn: null;
+    deletedBy: null;
+    remark: null;
 }
+
+
+
