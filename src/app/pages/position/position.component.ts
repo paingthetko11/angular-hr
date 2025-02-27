be313880b2e21654a2f480301e539c72e0727357
paingthetko11 <paingthetko081@gmail.com>
2025-02-27 14:00:54 +0630

    uc

---
+++ src/app/pages/position/position.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-position',
+  imports: [],
+  templateUrl: './position.component.html',
+  styleUrl: './position.component.scss'
+})
+export class PositionComponent {
+
+}
