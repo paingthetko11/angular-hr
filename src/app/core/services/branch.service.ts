0cc5b3245362f36eae992a0cfe96a938956ded0d
paingthetko11 <paingthetko081@gmail.com>
2025-02-27 13:55:31 +0630

    update code

---
+++ src/app/core/services/branch.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { RootModel } from '../models/root.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BranchService {
+
+  constructor(private http:HttpClient) { }
+   get(): Observable<RootModel> {
+      let url: string = `${environment.apiUrl}/api/HrBranch`;
+      return this.http.get<RootModel>(url);
+    }
+  
+    getbyID(id: number): Observable<RootModel> {
+      let url: string = `${environment.apiUrl}/api/HrBranch/${id}`;
+      return this.http.get<RootModel>(url);
+    }
+}
