8b0afa3855c9504c8e733d7fc43ec9c02dda0503
paingthetko11 <paingthetko081@gmail.com>
2025-02-28 12:41:48 +0630

    u.c

--- src/app/core/models/allowane.model.ts
+++ src/app/core/models/allowane.model.ts
@@ -15,3 +15,25 @@ export interface AllowaneModel {
   deletedBy: string | null;
   remark: string | null;
 }
+
+export interface ViAllowanceModel {
+  allowanceId: number;
+  allowanceName: string;
+  companyId: string;
+  companyName: string;
+  branchId: number;
+  branchName: string;
+  deptId: number;
+  deptName: string;
+  positionId: number;
+  positionName: string;
+  description: string;
+  status: boolean;
+  createdOn: string;
+  createdBy: string;
+  updatedOn: null;
+  updatedBy: null;
+  deletedOn: null;
+  deletedBy: null;
+  remark: null;
+}
