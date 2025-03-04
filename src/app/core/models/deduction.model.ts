eab101bb518229ee61bb76f2239b67dcab87c604
paingthetko11 <paingthetko081@gmail.com>
2025-03-04 15:47:13 +0630

    update code

--- src/app/core/models/deduction.model.ts
+++ src/app/core/models/deduction.model.ts
@@ -1,4 +1,4 @@
-export interface DeductionModel {
+export interface ViDeductionModel {
   deductionId: number;
   deductionName: string;
   companyId: string;
@@ -14,6 +14,23 @@ export interface DeductionModel {
   createdBy: string;
   updatedOn: null | string;
   updatedBy: null | string;
+  deletedOn: null;
+  deletedBy: null;
+  remark: null;
+}
+export interface DeductionModel {
+  deductionId: number;
+  companyId: string;
+  branchId: number;
+  deptId: number;
+  deductionName: string;
+  description: null;
+  isDefault: boolean;
+  status: boolean;
+  createdOn: null;
+  createdBy: null;
+  updatedOn: null;
+  updatedBy: null;
   deletedOn: null;
   deletedBy: null;
   remark: null;
