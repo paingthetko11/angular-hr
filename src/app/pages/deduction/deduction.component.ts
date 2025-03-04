ec73fa31c759585dbe7c88cc36aecdb168cedda6
paingthetko11 <paingthetko081@gmail.com>
2025-03-04 15:46:51 +0630

    update code

--- src/app/pages/deduction/deduction.component.ts
+++ src/app/pages/deduction/deduction.component.ts
@@ -1,11 +1,62 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputText, InputTextModule } from 'primeng/inputtext';
+import { PaginatorModule } from 'primeng/paginator';
+import { TableModule } from 'primeng/table';
+import {
+  DeductionModel,
+  ViDeductionModel,
+} from '../../core/models/deduction.model';
+import { DeductionService } from '../../core/services/deduction.service';
 
 @Component({
   selector: 'app-deduction',
-  imports: [],
-  templateUrl: './deduction.component.html',
-  styleUrl: './deduction.component.scss'
-})
-export class DeductionComponent {
+  imports: [
+    RouterModule,
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
 
+    //PrimeNg
+    TableModule,
+    PaginatorModule,
+    ButtonModule,
+    InputTextModule,
+    InputIconModule,
+  ],
+  templateUrl: './deduction.component.html',
+  styleUrl: './deduction.component.scss',
+})
+export class DeductionComponent implements OnInit {
+  deductions: ViDeductionModel[] = [];
+  selectedDecution!: ViDeductionModel;
+
+  isloading: boolean = false;
+
+  constructor(
+    private deductionSevice: DeductionService,
+    private route: Router
+  ) {}
+  ngOnInit(): void {}
+
+  loadata(): void {
+    this.deductionSevice.get().subscribe((res) => {
+      this.deductions = res.data as ViDeductionModel[];
+      this.isloading = false;
+      console.log(this.deductions);
+    });
+  }
+
+  // update(deductions:DeductionModel):void{
+  //   this.selectedDecution= deductions;
+
+  //   this.route.navigate([
+  //     'deduction/entry',
+  //     this.selectedDecution.deductionId,
+  //   ]);
+  // }
 }
