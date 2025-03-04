b4c191916d232c7b4c26416c52bcbe5325360f58
paingthetko11 <paingthetko081@gmail.com>
2025-03-04 14:46:48 +0630

    update code

---
+++ src/app/pages/deduction/deduction.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deduction',
+  imports: [],
+  templateUrl: './deduction.component.html',
+  styleUrl: './deduction.component.scss'
+})
+export class DeductionComponent {
+
+}
