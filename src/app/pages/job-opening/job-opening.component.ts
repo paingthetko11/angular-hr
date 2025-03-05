931cbd75eb1b92d91feb8422467c057e5af78afa
paingthetko11 <paingthetko081@gmail.com>
2025-03-05 16:57:59 +0630

    update code

---
+++ src/app/pages/job-opening/job-opening.component.ts
@@ -0,0 +1,56 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { RatingModule } from 'primeng/rating';
+import { SelectModule } from 'primeng/select';
+import { TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { JobOpeningService } from '../../core/services/job-opening.service';
+import { JobOpeningModel } from '../../core/models/job-opening.model';
+
+@Component({
+  selector: 'app-job-opening',
+  imports: [
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+
+    //primeNg
+    TableModule,
+    ButtonModule,
+    IconFieldModule,
+    InputIconModule,
+    InputTextModule,
+    TagModule,
+    RatingModule,
+    SelectModule,
+  ],
+  templateUrl: './job-opening.component.html',
+  styleUrl: './job-opening.component.scss',
+})
+export class JobOpeningComponent implements OnInit {
+  jobopenings: JobOpeningModel[] = [];
+  selectedJobOpening!: JobOpeningModel;
+  isLoading: boolean = false;
+
+  constructor(
+    private jobopeningService: JobOpeningService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loadata();
+  }
+  loadata(): void {
+    this.jobopeningService.get().subscribe((res) => {
+      this.jobopenings = res.data as JobOpeningModel[];
+      this.isLoading = false;
+    });
+  }
+}
